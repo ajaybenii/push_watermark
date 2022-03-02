90e698526fbdaf3bf669dce017ad3f8495bdcd91
Ajay Beniwal <85953534+ajaybenii@users.noreply.github.com>
2022-03-02 12:49:03 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ import aiohttp
 import requests
 from enum import Enum
 from io import BytesIO
-from typing import Optional
+from typing import ItemsView, List, Optional, final
 from urllib.parse import urlparse
 
 
